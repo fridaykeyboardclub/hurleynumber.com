fb97a3c64351accced12015edb1f14656369d554
Matt Snelling <mzsnelling@gmail.com>
2021-04-22 23:45:34 +0100

    Updates to messages from Griese

--- src/elm/Messages.elm
+++ src/elm/Messages.elm
@@ -1,29 +1,20 @@
 module Messages exposing (..)
 
-tooltip_all_keyboards = ""
-tooltip_all_switches = ""
-tooltip_all_keycaps = ""
-tooltip_all_accessories = ""
+tooltip_keyboards = "Total number of keyboards you own. A keyboard is a PCB and whatever it is mounted to/on/in (whether assembled or not). Stand-alone or extra PCBs are counted as accessories. Macropads and Num Pads are counted as keyboard kits; however, their switches and keycaps are not counted in their respective categories."
+tooltip_keyboard_gbs = "Total number of keyboards you currently have on order but have not yet received."
 
-tooltip_keyboards = "Total number of keyboards you own. A keyboard is a PCB and whatever it is mounted to/on/in (whether assembled or not). Stand alone or extra PCBs are counted as accessories. Macropads and Num Pads are counted as keyboard kits; however, their switches and keycaps are not counted in their respective categories."
-tooltip_keyboard_gbs = "Total number of keyboards you currenlty have on order but have not yet recieved."
-
-tooltip_unmodified_switches = "Total number of stock switch sets that you own. A set of switches sufficient to fill a normal sized board. Duplicate sets are counted if they were purchased separately or are used/intended to be used on separate boards."
-tooltip_modified_switches = "Total number of customized sets of switches that have been modified by you. Modified switch sets include thos that you have lubed, filmed or made other similar modifications to. Clipping switch pins does not count as a modification. If you make Frankenswitches (where you take components from one set of switches and use them to replace the components of another set of switches) both sets are considered modified if both sets have been reassembled. Each set of modified switches is counted as a single set regardless of the number of modifications made."
-tooltip_switch_gbs = "Total number of sets of switches you currenlty have on order but have not yet recieved."
+tooltip_unmodified_switches = "Total number of stock switch sets that you own. A set of switches sufficient to fill a normal-sized board. Duplicate sets are counted if they were purchased separately or are used/intended to be used on separate boards."
+tooltip_modified_switches = "Total number of customized sets of switches that have been modified by you. Modified switch sets include those that you have lubed, filmed, or made other similar modifications to. Clipping switch pins does not count as a modification. If you make Frankenswitches (where you take components from one set of switches and use them to replace the components of another set of switches) both sets are considered modified if both sets have been reassembled. Each set of modified switches is counted as a single set regardless of the number of modifications made."
+tooltip_switch_gbs = "Total number of sets of switches you currently have on order but have not yet received."
 
 tooltip_keycaps = "Total number of keycaps sets you own. If you have multiple kits for the same set they are all counted as one set unless they can be used to fully kit multiple boards in which case they are counted as a number of sets equal to the number of boards they can fully kit simultaneously. Artisan Keycaps are counted as Accessories"
-tooltip_keycap_gbs = "Total number of sets of keycaps you currenlty have on order but have not yet recieved."
+tooltip_keycap_gbs = "Total number of sets of keycaps you currently have on order but have not yet received."
 
 tooltip_artisans = "Total number of artisan keycaps you own. This does not include novelties."
-tooltip_artisan_gbs = "Total number of artisan keycaps you currenlty have on order but have not yet recieved"
-tooltip_deskpads = "Total number of deskmats you own."
-tooltip_deskpad_gbs = "Total number of deskmats you currenlty have on order but have not yet recieved"
+tooltip_artisan_gbs = "Total number of artisan keycaps you currently have on order but have not yet received"
+tooltip_deskpads = "Total number of desk mats you own."
+tooltip_deskpad_gbs = "Total number of desk mats you currently have on order but have not yet received"
 tooltip_cables = "Total number of 'fancy' cables you own."
-tooltip_cable_gbs = "Total number of 'fancy' cables you currenlty have on order but have not yet recieved"
-tooltip_accessories = "Total number of other keyboard accesories you own such as stand alone or spare PCBs, alternate plates, switch testers, etc."
-tooltip_accessory_gbs = "Total number of other keyboard accesories (spare PCBs, altenrate plates, switch testers, etc.) you currenlty have on order but have not yet recieved"
-
--- TODO more tooltip messages
-
--- TODO more tooltip messages
+tooltip_cable_gbs = "Total number of 'fancy' cables you currently have on order but have not yet received"
+tooltip_accessories = "Total number of other keyboard accessories you own such as stand-alone or spare PCBs, alternate plates, switch testers, etc."
+tooltip_accessory_gbs = "Total number of other keyboard accessories (spare PCBs, alternate plates, switch testers, etc.) you currently have on order but have not yet received"
